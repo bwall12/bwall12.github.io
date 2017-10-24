2b3d1788bdb85328a84eaa1d5c6dea57fe9b0726
bwall12 <32175093+bwall12@users.noreply.github.com>
2017-10-24 13:39:54 -0500

    Update my-script.js

--- JQueryInterface/Scripts/my-script.js
+++ JQueryInterface/Scripts/my-script.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
 		console.log('in my doc ready f/n');
 	$('#my-div-variable').addClass('my-border'); // when loaded, give it a border
 		console.log('in my doc ready f/n');
+	$('#my-div-date').addClass('my-border'); // when loaded, give it a border
+		console.log('in my doc ready f/n');
+	$('#my-div-year').addClass('my-border'); // when loaded, give it a border
+		console.log('in my doc ready f/n');
 
 	
 
