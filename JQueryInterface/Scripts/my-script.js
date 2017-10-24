1088291680d9d19488b16be57b4fe4d21dcf7446
bwall12 <32175093+bwall12@users.noreply.github.com>
2017-10-23 21:36:22 -0500

    d

---
+++ JQueryInterface/Scripts/my-script.js
@@ -0,0 +1,13 @@
+//i put everything in this function
+$(document).ready(function(){
+
+	$('#my-div-calendar').addClass('my-border'); // when loaded, give it a border
+		console.log('in my doc ready f/n');
+	$('#my-div-variable').addClass('my-border'); // when loaded, give it a border
+		console.log('in my doc ready f/n');
+
+	
+
+
+
+}); 
