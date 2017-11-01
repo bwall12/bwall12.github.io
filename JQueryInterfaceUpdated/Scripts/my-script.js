34c12cb848a17daea8002828278e2163b3a8adfa
bwall12 <32175093+bwall12@users.noreply.github.com>
2017-11-01 16:54:57 -0500

    Update my-script.js

--- JQueryInterfaceUpdated/Scripts/my-script.js
+++ JQueryInterfaceUpdated/Scripts/my-script.js
@@ -1,28 +1,28 @@
 //i put everything in this function
 $(document).ready(function(){
-
-	$('#my-div-calendar').addClass('my-border'); // when loaded, give it a border
-		console.log('in my doc ready f/n');
-	$('#my-div-variable').addClass('my-border'); // when loaded, give it a border
-		console.log('in my doc ready f/n');
-	$('#my-div-date').addClass('my-border'); // when loaded, give it a border
-		console.log('in my doc ready f/n');
-	$('#my-div-year').addClass('my-border'); // when loaded, give it a border
-		console.log('in my doc ready f/n');
-	$('#my-div-console').addClass('my-border'); // when loaded, give it a border
-		console.log('in my doc ready f/n');
-
-
-
-	$('#my-div-console').append('Console is ready...');
 	
-	$('th').click(function(){
-		$('#my-div-console').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
-		$(this).toggleClass('selected').siblings().removeClass('selected');
-});
-
+		$('#my-div-calendar').addClass('my-border'); // when loaded, give it a border
+			console.log('in my doc ready f/n');
+		$('#my-div-variable').addClass('my-border'); // when loaded, give it a border
+			console.log('in my doc ready f/n');
+		$('#my-div-date').addClass('my-border'); // when loaded, give it a border
+			console.log('in my doc ready f/n');
+		$('#my-div-year').addClass('my-border'); // when loaded, give it a border
+			console.log('in my doc ready f/n');
+		$('#my-div-console').addClass('my-border'); // when loaded, give it a border
+			console.log('in my doc ready f/n');
 	
-
-
-
-}); 
+	
+	
+		$('#my-div-console').append('Console is ready...');
+		
+		$('th').click(function(){
+			$('#my-div-console').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
+			$(this).toggleClass('selected').siblings().removeClass('selected');
+	});
+	
+		
+	
+	
+	
+	}); 
